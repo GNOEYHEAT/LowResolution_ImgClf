d05a7732573fd91331ab32e03aa683dcb3d47ed5
jeongHwarr <green2368@naver.com>
2024-05-18 15:51:01 +0900

    MODIFY: main default args

--- main.py
+++ main.py
@@ -9,16 +9,14 @@ from scripts.train import train
 from scripts.submission import submission
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Run files [train.py, test.py]')
+    parser = argparse.ArgumentParser(description='Run files [scripts/train.py, scripts/submission.py]')
 
     parser.add_argument('--cfg_path', default=f"{ROOT_DIR}/configs/dinov2-large.yaml", type=str,
                         help="Path to the config file in yaml format")
-    parser.add_argument('--save_path', default="results/dinov2-large_240518", type=str,
+    parser.add_argument('--save_path', default="results/dinov2-large", type=str,
                         help="Path to the saved models")
-    parser.add_argument('--num_workers', default=2, type=int, help="Number of "
+    parser.add_argument('--num_workers', default=0, type=int, help="Number of "
                         "workers for each data loader")
-    # parser.add_argument('--device', default='0', type=str,
-    #                     help="GPU indices ""comma separated, e.g. '0,1' ")
     parser.add_argument('--device', nargs='+', default=[0], type=int,
                         help="GPU indices")
     return parser.parse_args()
